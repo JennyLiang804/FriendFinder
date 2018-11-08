7b8155ec1bf28ed160e71fb0e6d34367038c21fa
Jenny Liang <jennylove804@gmail.com>
2018-11-08 16:43:53 -0500

    testing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,19 @@
 var friends = [
     {
-        name:"",
-        photo:"",
-        scores: []
+        name:"Grinch",
+        photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiLub2XzMXeAhUhT98KHTZHAJkQjRx6BAgBEAU&url=https%3A%2F%2Fiz.ru%2F718034%2F2018-03-09%2Fv-seti-poiavilsia-treiler-multfilma-grinch&psig=AOvVaw3PUdg1WzjvZPZ9lpNF6B2c&ust=1541793940898235",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+
+    {
+        name: "Christmas Cat",
+        photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjs-pqw0cXeAhWomuAKHbgPDJYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.facebook.com%2FThePlayfulPainters%2Fphotos%2Fa.237029566656663%2F723172921375656%2F%3Ftype%3D3&psig=AOvVaw39mUAk7gKq84pFBsibtMML&ust=1541795265987771",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+
+    {
+        name: "Cindy Lou Who",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiBopLazcXeAhXmYN8KHdFiA_UQjRx6BAgBEAU&url=http%3A%2F%2Fzahradni-nabytek-vyprodej.info%2Fgrinch-who-stole-christmas-girlfriend.html&psig=AOvVaw21P7R908BifAgu3dJNm_2v&ust=1541794303490258",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     }
 ]
